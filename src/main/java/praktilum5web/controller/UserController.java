9de97e455f88d3e881e449a24f39a9ee48c23763
Iaannn06 <161089330+Iaannn06@users.noreply.github.com>
2026-04-06 14:25:55 +0700

    menambahkan UserController

--- src/main/java/praktilum5web/controller/UserController.java
+++ src/main/java/praktilum5web/controller/UserController.java
@@ -1,9 +1,36 @@
 package praktilum5web.controller;
 
-import ch.qos.logback.core.model.Model;
+import org.springframework.ui.Model;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+@Controller
 public class UserController {
+    private final String USERNAME = "admin";
+    private final String PASSWORD = "20230140126";
+
+    @GetMapping("/")
+    public String loginPage() {
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestParam String username,
+                        @RequestParam String password,
+                        Model model) {
+
+        if (username.equals(USERNAME) && password.equals(PASSWORD)) {
+            return "home";
+        } else {
+            model.addAttribute("error", "Username atau password salah!");
+            return "login";
+        }
+    }
+
+    @GetMapping("/home")
+    public String home() {
+        return "home";
+    }
 }
