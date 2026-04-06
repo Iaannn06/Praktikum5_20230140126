308e83aad0f451b63832ac7fa3097e870f03c068
Iaannn06 <161089330+Iaannn06@users.noreply.github.com>
2026-04-06 14:10:44 +0700

    Initial commit

---
+++ src/main/java/praktilum5web/controller/UserController.java
@@ -0,0 +1,9 @@
+package praktilum5web.controller;
+
+import ch.qos.logback.core.model.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+public class UserController {
+}
